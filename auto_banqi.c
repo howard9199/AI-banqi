79b5c244af3f6d2ab4bf834b9e012e35552921c7
howard9199 <53228984+howard9199@users.noreply.github.com>
2021-12-08 23:12:17 +0800

    move some function definitions to auto_banqi.c

--- auto_banqi.c
+++ auto_banqi.c
@@ -4,6 +4,10 @@
 #include <time.h>
 #include "auto_banqi.h"
 
+void auto_traversal_move(int32_t player, int32_t x, int32_t y);
+static int32_t auto_legal_move(int32_t player, int32_t x, int32_t y, int32_t tar_x, int32_t tar_y);
+static int32_t auto_can_eat(char a, char b);
+
 static char chess[4][8];
 static char color[2] = {'x','x'};
 
--- auto_banqi.h
+++ auto_banqi.h
@@ -2,7 +2,4 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void auto_traversal_move(int32_t player, int32_t x, int32_t y);
-static int32_t auto_legal_move(int32_t player, int32_t x, int32_t y, int32_t tar_x, int32_t tar_y);
-static int32_t auto_can_eat(char a, char b);
 int* auto_move(int32_t player, char chess[][8], char each_color[]);
